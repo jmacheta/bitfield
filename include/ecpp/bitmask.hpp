bb0dca80f05b82eb3dce16e188885e3fe0bd3e1c
Jan Macheta <jan.macheta@embe.tech>
2023-09-22 14:45:35 +0200

    added bitmask constructor. minor documentation fixes

--- include/ecpp/bitmask.hpp
+++ include/ecpp/bitmask.hpp
@@ -20,11 +20,21 @@ namespace ecpp {
         using value_type = std::remove_cv_t<T>;
         value_type value; ///< Actual value of bitmask
 
+        constexpr explicit(false) bitmask(value_type v) noexcept : value{v} {
+        }
 
+        /**
+         * Returns number of trailing zeros in bitmask value (counting from LSB)
+         * @return Number of trailing zeros
+         */
         constexpr int trailing_zeros() const noexcept {
             return std::countr_zero(value);
         }
 
+        /**
+         * Returns number of leading zeros in bitmask value (counting from MSB)
+         * @return Number of leading zeros or 0 when value is 0
+         */
         constexpr int leading_zeros() const noexcept {
             if (std::cmp_equal(value, 0U)) [[unlikely]] {
                 return 0;
@@ -32,10 +42,18 @@ namespace ecpp {
             return std::countl_zero(value);
         }
 
+        /**
+         * Returns number of bits set in bitmask value
+         * @return Number of bits set
+         */
         constexpr int popcount() const noexcept {
             return std::popcount(value);
         }
 
+        /**
+         * Returns width of bitmask value, i.e. distance between first and last set bit
+         * @return Width of bitmask
+         */
         constexpr int width() const noexcept {
             if (std::cmp_equal(value, 0U)) [[unlikely]] {
                 return 0;
@@ -43,6 +61,10 @@ namespace ecpp {
             return std::numeric_limits<value_type>::digits - (trailing_zeros() + leading_zeros());
         }
 
+        /**
+         * Returns base value of bitmask, i.e. value with all trailing zeros removed
+         * @return Base value of bitmask
+         */
         constexpr value_type base_value() const noexcept {
             return static_cast<value_type>(value >> trailing_zeros());
         }
